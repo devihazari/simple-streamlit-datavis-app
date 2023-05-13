2a86f27278f90e2b74795a23ba70318247e37f8d
Devi <devi@Aishwaryas-MacBook-Pro.local>
2023-05-13 15:52:35 -0300

    Removed null values and added scatter plot

--- app.py
+++ app.py
@@ -2,16 +2,54 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 import plotly.graph_objects as go
+import numpy as np
 
 df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+
+# replace null values in the 'is_4wd' column with the value 0
+df['is_4wd'].fillna(value=0, inplace=True)
+df['is_4wd'] = df['is_4wd'].astype('int')
+
+
+# compute the median of non-null values in the 'model_year' column for each model group
+medians = df.groupby('model')['model_year'].transform(lambda x: x.median())
+# replace null values in the 'model_year' column with the median of non-null values for each model group
+df['model_year'].fillna(medians, inplace=True)
+
+
+# compute the median of non-null values in the 'cylinders' column for each model group
+medians = df.groupby('model')['cylinders'].transform(lambda x: x.median())
+# replace null values in the 'cylinders' column with the median of non-null values for each model group
+df['cylinders'].fillna(medians, inplace=True)
+
+
+# compute the median of non-null values in the 'odometer' column for each model_year group
+medians = df.groupby('model_year')['odometer'].transform(lambda x: x.median())
+# replace null values in the 'odometer' column with the median of non-null values for each model+year group
+df['odometer'].fillna(medians, inplace=True)
+
+
+# replace null values in the 'paint_color' column with the value 'NA'
+df['paint_color'].fillna(value='NA', inplace=True)
+
+df['model_year'] = df['model_year'].astype('int')
+df['cylinders'] = df['cylinders'].astype('int')
+
 
 st.header('Data viewer')
 show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
 if not show_manuf_1k_ads:
     df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
-
 st.dataframe(df)
+
+
+st.header('Scatter plot of `model_year` vs `price`')
+# Create a scatter plot using plotly_express
+st.write(px.scatter(df, x='model_year', y='price'))
+
+
 st.header('Vehicle types by fuel')
 st.write(px.histogram(df, x='manufacturer', color='fuel'))
 st.header('Histogram of `transmission` vs `model_year`')
